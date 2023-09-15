a232f47d3066a20bbdfbd9ac4c3f2df80b81cbc5
kumikoumehara_stitch <69409615+ume526@users.noreply.github.com>
2023-09-15 16:07:11 +0900

    交通広告にイベントスペースを追加

--- resources/views/transit-advertising/index.blade.php
+++ resources/views/transit-advertising/index.blade.php
@@ -1,10 +1,10 @@
 ﻿<!DOCTYPE html>
 @include('layouts.meta_head')
 <title>JR東日本交通広告｜JR東日本東北総合サービス株式会社【LiViT】</title>
-<meta name="description" content="JR東日本グループ力を活かし、仙台駅エキナカを中心としたJR主要駅の交通広告掲出を、お客様のご計画・ご要望に沿って幅広く展開。印刷設備完備で、各種サインの制作から施工まで一貫したサービスも行い、交通広告だけでなく、パンフレットやチラシ等の印刷対応も可能です。">
+<meta name="description" content="JR東日本のグループ力を活かし、仙台駅エキナカを中心としたJR主要駅の交通広告掲出を、お客様のご計画・ご要望に沿って幅広く展開。制作から施工まで一貫したサービスも行い、交通広告だけでなく、デザイン制作や様々なサインの製作、イベントの造作・設営、パンフレットやチラシを始め各種印刷物、ラッピング等も対応可能です。">
 <meta name="keywords" content="JR,ジェイアール,東北,宮城,仙台,岩手,青森,秋田,山形,福島,駅,エキナカ,構内,交通広告,広告,メディア,デジタルサイネージ,車両メディア,駅メディア,媒体,媒体制作,媒体企画,申し込み,掲載,掲出,制作,作成,問合せ,申し込み">
 <meta property="og:title" content="JR東日本交通広告｜JR東日本東北総合サービス株式会社【LiViT】">
-<meta property="og:description" content="JR東日本グループ力を活かし、仙台駅エキナカを中心としたJR主要駅の交通広告掲出を、お客様のご計画・ご要望に沿って幅広く展開。印刷設備完備で、各種サインの制作から施工まで一貫したサービスも行い、交通広告だけでなく、パンフレットやチラシ等の印刷対応も可能です。">
+<meta property="og:description" content="JR東日本のグループ力を活かし、仙台駅エキナカを中心としたJR主要駅の交通広告掲出を、お客様のご計画・ご要望に沿って幅広く展開。制作から施工まで一貫したサービスも行い、交通広告だけでなく、デザイン制作や様々なサインの製作、イベントの造作・設営、パンフレットやチラシを始め各種印刷物、ラッピング等も対応可能です。">
 <meta property="og:url" content="{{ url()->full() }}">
 @include('layouts.meta_bottom')
 <!-- [start]css -->
@@ -43,32 +43,37 @@
 														<p class="mail"><a href="mailto:koukokug@livit.jregroup.ne.jp">koukokug@livit.jregroup.ne.jp</a></p>
 												</dd>
 										</dl>
-										<p class="lead">JR東日本グループ力を活かし、仙台駅エキナカを中心としたJR主要駅の交通広告掲出を、<br>
+										<p class="lead">JR東日本のグループ力を活かし、仙台駅エキナカを中心としたJR主要駅の交通広告掲出を、<br class="pc">
 												お客様のご計画・ご要望に沿って幅広く展開しております。<br>
-												印刷設備完備で、各種サインの制作から施工まで一貫したサービスも行っており、<br>
-												交通広告だけでなく、パンフレットやチラシ等の印刷対応も可能です。 </p>
+												制作から施工まで一貫したサービスも行っており、<br class="pc">
+												交通広告だけでなく、デザイン制作や様々なサインの製作、イベントの造作・設営、<br class="pc">パンフレットやチラシを始め各種印刷物、ラッピング等も対応可能です。 </p>
 								</div>
-							<section class="contents">
+								<section class="contents">
 										<ul class="advert">
 												<li>
 														<div class="photo"><a href="/serviceinfo/assets/pdf/transit-advertising/digital.pdf" target="_blank"><img src="/serviceinfo/assets/img/transit-advertising/img01.jpg" alt=""></a></div>
 														<h3>デジタルサイネージ</h3>
-														<p>多くの人が行きかう駅構内に動画・静止画による<br>デジタル広告媒体。</p>
+														<p>多くの人が行き交う駅構内に適しており、<br class="pc">注目度がとても高い動画・静止画によるデジタル広告媒体です。</p>
+												</li>
+												<li>
+														<div class="photo"><a href="/serviceinfo/assets/pdf/transit-advertising/event.pdf" target="_blank"><img src="/serviceinfo/assets/img/transit-advertising/img05.jpg" alt=""></a></div>
+														<h3>イベントスペース</h3>
+														<p>お客さま流動の多い駅構内のイベントスペースは、プロモーション効果がとても高く、幅広い層へのダイレクトな訴求に適しています。</p>
 												</li>
 												<li>
 														<div class="photo"><a href="/serviceinfo/assets/pdf/transit-advertising/station.pdf" target="_blank"><img src="/serviceinfo/assets/img/transit-advertising/img02.jpg" alt=""></a></div>
 														<h3>駅メディア</h3>
-														<p>駅を利用する人々へ幅広く情報を提供。視覚的にインパクトのある<br>訴求やタイムリーな情報の訴求に適しています。</p>
+														<p>駅を利用する人々へ幅広く情報を提供。視覚的にインパクトのある<br class="pc">訴求やタイムリーな情報の訴求に適しています。</p>
 												</li>
 												<li>
 														<div class="photo"><a href="/serviceinfo/assets/pdf/transit-advertising/vehicle.pdf" target="_blank"><img src="/serviceinfo/assets/img/transit-advertising/img03.jpg" alt=""></a></div>
 														<h3>車両メディア</h3>
-														<p>ＪＲ車両ならではの広告媒体が多数存在し、<br>企業や商品のニーズに合った媒体を選択できます。</p>
+														<p>ＪＲ車両ならではの広告媒体が多数存在し、<br class="pc">企業や商品のニーズに合った媒体を選択できます。</p>
 												</li>
 												<li>
 														<div class="photo"><a href="/serviceinfo/assets/pdf/transit-advertising/advert.pdf" target="_blank"><img src="/serviceinfo/assets/img/transit-advertising/img04.jpg" alt=""></a></div>
 														<h3>広告媒体企画・制作</h3>
-														<p>広告デザインや各種サイン、横断幕・懸垂幕、ラッピング、印刷物など<br>様々な広告媒体の企画・制作をご提案いたします。</p>
+														<p>広告デザインや各種サイン、横断幕・懸垂幕、ラッピング、印刷物など<br class="pc">様々な広告媒体の企画・制作をご提案いたします。</p>
 												</li>
 										</ul>
 										<p class="small">資料提供：(株)ジェイアール東日本企画仙台支社</p>
@@ -76,8 +81,7 @@
 								<section class="contact">
 										<div class="inner">
 												<h2 class="tit">お問合せ</h2>
-												<p class="lead">お見積もりなど、<br class="sp">
-														お気軽にご相談ください。</p>
+												<p class="lead">広告や制作のお見積りや、媒体の空き状況の確認など、お気軽にご相談ください。</p>
 												<div class="box">
 														<h3 class="tit">メディアサービスセンター</h3>
 														<dl class="accept">
