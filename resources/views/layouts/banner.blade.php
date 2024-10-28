86d0320fb470fd2a3f8a154040a060f08e0283ef
Umehara <kumiko.umehara@stitch.co.jp>
2024-10-28 18:57:57 +0900

    バナーリンク修正

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -4,9 +4,9 @@
 			<div class="tpl_linkBox">
 				<ul class="tpl_linkList">
 					<li><a href="https://www.jrepoint.jp/" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_01.jpg" alt="JRE POINT" height="81" width="321"></a></li>
-					<li><a href="https://www.jreastmall.com/shop/c/c13/?utm_source=livithp_u&utm_medium=shoptop_banner" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_02.jpg" alt="東北MONO" height="81" width="321"></a></li>
-					<li><a href="https://furusato.jrenet.jp/furusato?utm_source=livit.jregroup.ne.jp&utm_medium=referral&utm_campaign=jgrf_202307_livit_furusato" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_03.jpg" alt="JRE MALL ふるさと納税" height="81" width="321"></a></li>
-					<li><a href="https://media.jrenet.jp/?utm_source=backlinkSEO&utm_medium=distribution_kit" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_04.png" alt="JR東日本の鉄道おでかけメディアJRE MALL Media" height="81" width="321"></a></li>
+					<li><a href="https://shopping.jreast.co.jp/shop/detail/s013?utm_source=livithp_u&utm_medium=shoptop_banner" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_02.jpg" alt="東北MONO" height="81" width="321"></a></li>
+					<li><a href="https://furusato.jreast.co.jp/furusato?utm_source=livit.jregroup.ne.jp&utm_medium=referral&utm_campaign=jgrf_202307_livit_furusato" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_03.jpg" alt="JRE MALL ふるさと納税" height="81" width="321"></a></li>
+					<li><a href="https://media.jreast.co.jp/" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_04.png" alt="JR東日本の鉄道おでかけメディアJRE MALL Media" height="81" width="321"></a></li>
 					<li><a href="https://yorimichi-sendai.jp/" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_05.jpg" alt="ヨリ未知 SENDAI" height="81" width="321"></a></li>
 					<li><a href="http://www.jreast.co.jp/suicamoney/index.html/" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_06.png" alt="Suica" height="81" width="321"></a></li>
 					<li><a href="https://www.jreast.co.jp/card/" target="_blank" class="tpl_btn"><img src="/serviceinfo/assets/img/common/bnr_foot_related_07.jpg" alt="VIEW CARD" height="81" width="321"></a></li>
