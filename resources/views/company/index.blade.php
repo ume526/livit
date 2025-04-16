ffada9d3d8a7597738e68d1fb31c4c9e9aef379f
Umehara <kumiko.umehara@stitch.co.jp>
2025-04-16 09:54:50 +0900

    【2025_0331公開】社員募集バナー差し替え、牛たんLP更新

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -84,8 +84,7 @@
                        <div class="key_visual standard-box-wrap">
                                 <ul id="main-slider" class="main-slider">
 								        <li><a href="/company/livit35th/"><img src="/assets/img/company/index/keyvisual_img11.jpg" alt="35th Anniversary"></a></li>
-                                        <li><a href="/company/recruit/guideline/"><img src="/assets/img/company/index/keyvisual_img09.jpg" alt="LiViT 社員募集"></a></li>
-                                        <li><a href="https://www.livit.jregroup.ne.jp/gyutan-sushistreet/"><img src="/assets/img/company/index/keyvisual_img10.jpg" alt="牛たん通りす
+                                        <li><a href="/company/recruit/guideline/"><img src="/assets/img/company/index/keyvisual_img09.png" alt="LiViT 社員募集"></a></li>
 し通り"></a></li>
                                         <li><a href="https://www.livit.jregroup.ne.jp/search?keywords=%E3%81%84%E3%82%8D%E3%81%A8%E3%81%84%E3%82%8D"><img src="/assets/img/company/index/keyvisual_img01.jpg" alt="いろといろ"></a></li>
 
@@ -94,7 +93,7 @@
 
                                 <ul id="thumbnail-slider" class="thumbnail-slider">
 								<li><a href="/company/livit35th/"><img src="/assets/img/company/index/keyvisual_img11.jpg" alt="35th Anniversary"></a></li>
-					<li><a href="/company/recruit/guideline/"><img src="/assets/img/company/index/keyvisual_img09.jpg" alt="LiViT 社員募集"></a></li>
+					<li><a href="/company/recruit/guideline/"><img src="/assets/img/company/index/keyvisual_img09.png" alt="LiViT 社員募集"></a></li>
 					<li><a href="https://www.livit.jregroup.ne.jp/gyutan-sushistreet/"><img src="/assets/img/company/index/keyvisual_img10.jpg" alt="牛たん通りす
 し通り"></a></li>
 					<li><a href="https://www.livit.jregroup.ne.jp/search?keywords=%E3%81%84%E3%82%8D%E3%81%A8%E3%81%84%E3%82%8D"><img src="/assets/img/company/index/keyvisual_img01.jpg" alt="いろといろ"></a></li>
