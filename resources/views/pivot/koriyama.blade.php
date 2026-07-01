289e0191aac36059aa924bbd7bd0ee70899284dd
Umehara <kumiko.umehara@stitch.co.jp>
2026-07-01 15:33:26 +0900

    【2026.7.1公開】企業ページ・pivot更新

--- resources/views/pivot/koriyama.blade.php
+++ resources/views/pivot/koriyama.blade.php
@@ -242,8 +242,8 @@ JR東日本グループの<br>
 								<section class="park">
 										<div class="inner">
 													<h2 class="tit">PARKING | 駐車場</h2>
-												<div class="place"> <img src="/serviceinfo/assets/img/pivot/koriyama_park_pc.gif" class="pc" alt="駐車場"><img src="/serviceinfo/assets/img/pivot/koriyama_park_sp.gif" class="sp" alt="駐車場"> </div>
-												<p class="note">2026年7月1日より、駐車場の最大料金が変更となります。詳しくはNEWS・EVENTページをご確認いただきますようお願いいたします。</p>
+												<div class="place"> <img src="/serviceinfo/assets/img/pivot/koriyama_park_pc.png" class="pc" alt="駐車場"><img src="/serviceinfo/assets/img/pivot/koriyama_park_sp.png" class="sp" alt="駐車場"> </div>
+												<!-- <p class="note">2026年7月1日より、駐車場の最大料金が変更となります。詳しくはNEWS・EVENTページをご確認いただきますようお願いいたします。</p> -->
 												<ul class="bgItem">
 														<li class="item15 sp"></li>
 												</ul>
